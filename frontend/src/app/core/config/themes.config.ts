244a2f1d72c2822ea9177f8b36157ee185af8fcf
hegedus-mark <hegedusmark2018@gmail.com>
2024-12-22 21:18:46 +0100

    chore: Add themes.config file

---
+++ frontend/src/app/core/config/themes.config.ts
@@ -0,0 +1,52 @@
+export interface ThemesConfig {
+  id: string;
+  name: string;
+  icon?: string;
+}
+
+// Helper function to capitalize first letter
+function capitalize(str: string): string {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+// Create array of theme IDs
+const themeIds = [
+  "light",
+  "dark",
+  "cupcake",
+  "bumblebee",
+  "emerald",
+  "corporate",
+  "synthwave",
+  "retro",
+  "cyberpunk",
+  "valentine",
+  "halloween",
+  "garden",
+  "forest",
+  "aqua",
+  "lofi",
+  "pastel",
+  "fantasy",
+  "wireframe",
+  "black",
+  "luxury",
+  "dracula",
+  "cmyk",
+  "autumn",
+  "business",
+  "acid",
+  "lemonade",
+  "night",
+  "coffee",
+  "winter",
+  "dim",
+  "nord",
+  "sunset",
+] as const;
+
+// Transform into ThemesConfig array
+export const THEMES_CONFIG: ThemesConfig[] = themeIds.map(id => ({
+  id,
+  name: id === 'cmyk' ? 'CMYK' : capitalize(id), // Special case for CMYK
+}));
