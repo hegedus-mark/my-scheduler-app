a7f19b05d147891463878da25d9a357396004d18
hegedus-mark <hegedusmark2018@gmail.com>
2024-12-23 22:58:58 +0100

    feat: Add main-layout and side-bar

--- frontend/src/app/core/layout/main-layout/main-layout.component.ts
+++ frontend/src/app/core/layout/main-layout/main-layout.component.ts
@@ -1,10 +1,14 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { SidebarComponent } from "@core/layout/sidebar/sidebar.component";
+import { navigationConfig } from "@core/config/navigation.config";
 
 @Component({
   selector: "app-main-layout",
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: "./main-layout.component.html",
   styleUrl: "./main-layout.component.scss",
 })
-export class MainLayoutComponent {}
+export class MainLayoutComponent {
+  protected readonly navigationConfig = navigationConfig;
+}
--- frontend/src/app/core/layout/sidebar/sidebar.component.ts
+++ frontend/src/app/core/layout/sidebar/sidebar.component.ts
@@ -1,9 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, input } from "@angular/core";
+import { RouterLink, RouterLinkActive } from "@angular/router";
+import { NgOptimizedImage } from "@angular/common";
+import { NavItem } from "@core/config/navigation.config";
 
 @Component({
   selector: "app-sidebar",
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, NgOptimizedImage],
   templateUrl: "./sidebar.component.html",
   styleUrl: "./sidebar.component.scss",
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  navigationItems = input<NavItem[]>([]);
+}
