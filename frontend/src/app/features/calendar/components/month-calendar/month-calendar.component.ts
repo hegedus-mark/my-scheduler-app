4e05fa05741cc89f3c5e5f6d5e56df2e305d9c86
hegedus-mark <hegedusmark2018@gmail.com>
2024-12-29 12:57:20 +0100

    refactor: Change monthCalendar to use CalendarService

--- frontend/src/app/features/calendar/components/month-calendar/month-calendar.component.ts
+++ frontend/src/app/features/calendar/components/month-calendar/month-calendar.component.ts
@@ -1,15 +1,11 @@
-import {
-  Component,
-  computed,
-  DestroyRef,
-  inject,
-  OnInit,
-  signal,
-} from "@angular/core";
+import { Component, computed, inject } from "@angular/core";
 import { LucideAngularModule, Plus } from "lucide-angular";
 import { MonthCalendarCell } from "@features/calendar/components/month-calendar/month-calendar.types";
-import { ActivatedRoute, Router } from "@angular/router";
-import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import {
+  DAY_CELLS_IN_MONTH,
+  DAYS_OF_WEEK,
+} from "@features/calendar/constants/calendar.constants";
+import { CalendarService } from "@features/calendar/services/calendar.service";
 
 @Component({
   selector: "app-month-calendar-calendar",
@@ -17,49 +13,13 @@ import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
   templateUrl: "./month-calendar.component.html",
   styleUrl: "./month-calendar.component.scss",
 })
-export class MonthCalendarComponent implements OnInit {
-  private readonly route = inject(ActivatedRoute);
-  private readonly router = inject(Router);
-  private readonly destroyRef = inject(DestroyRef);
+export class MonthCalendarComponent {
+  private readonly calendarService = inject(CalendarService);
 
-  readonly DaysOfWeek = [
-    "Sun",
-    "Mon",
-    "Tue",
-    "Wed",
-    "Thu",
-    "Fri",
-    "Sat",
-  ] as const;
-  private readonly CELLS = 42; // 6 * 7
-
-  readonly currentDate = signal(new Date());
+  readonly currentDate = this.calendarService.currentDate;
   readonly currentDaysInMonth = computed(() =>
     this.getDaysInMonth(this.currentDate()),
   );
-
-  ngOnInit() {
-    // Subscribe to query params to get the date
-    this.route.queryParams
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((params) => {
-        if (params["date"]) {
-          this.currentDate.set(new Date(params["date"]));
-        }
-      });
-  }
-
-  // Update navigation methods to use routing
-  handleMonthChange = (offset: number) => {
-    const newDate = new Date(this.currentDate());
-    newDate.setMonth(newDate.getMonth() + offset);
-
-    this.router.navigate([], {
-      relativeTo: this.route,
-      queryParams: { date: newDate.toISOString() },
-      queryParamsHandling: "merge",
-    });
-  };
 
   private getDaysInMonth = (date: Date) => {
     const year = date.getFullYear();
@@ -69,7 +29,7 @@ export class MonthCalendarComponent implements OnInit {
     const startingDay = firstDay.getDay();
     const daysInMonth = lastDay.getDate();
 
-    const result = new Array<MonthCalendarCell>(this.CELLS);
+    const result = new Array<MonthCalendarCell>(DAY_CELLS_IN_MONTH);
 
     //prev month-calendar
     const prevMonth = new Date(year, month - 1);
@@ -96,7 +56,7 @@ export class MonthCalendarComponent implements OnInit {
     //next month-calendar
     const nextMonth = new Date(year, month + 1);
     let nextMonthDay = 1;
-    for (let i = startingDay + daysInMonth; i < this.CELLS; i++) {
+    for (let i = startingDay + daysInMonth; i < DAY_CELLS_IN_MONTH; i++) {
       result[i] = {
         currentMonth: false,
         date: new Date(
@@ -111,4 +71,5 @@ export class MonthCalendarComponent implements OnInit {
   };
 
   readonly Plus = Plus;
+  readonly DAYS_OF_WEEK = DAYS_OF_WEEK;
 }
---
+++ frontend/src/app/features/calendar/constants/calendar.constants.ts
@@ -0,0 +1,10 @@
+export const DAYS_OF_WEEK = [
+  "Sun",
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat",
+] as const;
+export const DAY_CELLS_IN_MONTH = 42 as const; // 6 * 7
