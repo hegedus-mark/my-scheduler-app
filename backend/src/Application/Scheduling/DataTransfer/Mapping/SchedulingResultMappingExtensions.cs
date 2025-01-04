ca92a2be7efcc8bd6cb8baff6907b253f998b0c4
hegedus-mark <hegedusmark2018@gmail.com>
2025-01-04 14:57:21 +0100

    feat: Add mapping for SchedulingResult

--- backend/src/Application/Scheduling/DataTransfer/Mapping/SchedulingResultMappingExtensions.cs
+++ backend/src/Application/Scheduling/DataTransfer/Mapping/SchedulingResultMappingExtensions.cs
@@ -1,3 +1,25 @@
+using Application.Scheduling.DataTransfer.DTOs;
+using Scheduling.Domain.Results;
+
 namespace Application.Scheduling.DataTransfer.Mapping;
 
-public static class SchedulingResultMappingExtensions { }
+public static class SchedulingResultMappingExtensions
+{
+    public static SchedulingResultDto ToDto(this SchedulingResult schedulingResult)
+    {
+        return new SchedulingResultDto
+        {
+            FailedTasks = schedulingResult.FailedTasks.Select(t => t.ToDto()).ToList(),
+            ScheduledTasks = schedulingResult.ScheduledTasks.Select(t => t.ToDto()).ToList(),
+            HasFailedTasks = schedulingResult.HasFailedTasks,
+        };
+    }
+
+    public static SchedulingResult ToDomain(this SchedulingResultDto dto)
+    {
+        return SchedulingResult.Create(
+            dto.ScheduledTasks.Select(t => t.ToDomain()),
+            dto.FailedTasks.Select(t => t.ToDomain())
+        );
+    }
+}
