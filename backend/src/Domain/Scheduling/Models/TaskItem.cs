deeda902a1734a42496ea6ec5e6031acb16f63da
hegedus-mark <hegedusmark2018@gmail.com>
2025-01-04 23:37:55 +0100

    chore: Change using in TaskItem

--- backend/src/Domain/Scheduling/Models/TaskItem.cs
+++ backend/src/Domain/Scheduling/Models/TaskItem.cs
@@ -2,9 +2,9 @@ using Domain.Scheduling.Events;
 using Domain.Scheduling.Models.Enums;
 using Domain.Scheduling.Models.TaskStates;
 using Domain.Scheduling.Services;
+using Domain.Shared.Base;
+using Domain.Shared.ValueObjects;
 using SharedKernel.Common.Guard;
-using AggregateRoot = Domain.Shared.Base.AggregateRoot;
-using CalendarTimeWindow = Domain.Shared.ValueObjects.CalendarTimeWindow;
 
 namespace Domain.Scheduling.Models;
 
