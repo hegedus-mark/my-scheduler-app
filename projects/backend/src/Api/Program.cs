a39868a87dad38d35ddcd9acc8bc79c0b88935fa
hegedus-mark <hegedusmark2018@gmail.com>
2025-02-25 20:47:08 +0100

    feat: Add draftTask migration

--- projects/backend/src/Api/Program.cs
+++ projects/backend/src/Api/Program.cs
@@ -1,7 +1,11 @@
 using System.Text.Json.Serialization;
 using Api.Configuration;
 using Api.Controllers.DraftTasks;
+using Application.DraftTasks;
 using Application.DraftTasks.CreateDraftTask;
+using Application.DraftTasks.DeleteDraftTask;
+using Application.DraftTasks.GetAllDraftTasks;
+using Application.DraftTasks.UpdateDraftTask;
 using dotenv.net;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -36,11 +40,20 @@ builder.Services.AddCors(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.ConfigureProblemDetails();
 
-//Services
+//DraftTaskServices
 builder.Services.AddScoped<ICreateDraftTaskService, CreateDraftTaskService>();
+builder.Services.AddScoped<IUpdateDraftTaskService, UpdateDraftTaskService>();
+builder.Services.AddScoped<IGetAllDraftTasksService, GetAllDraftTasksService>();
+builder.Services.AddScoped<IDeleteDraftTaskService, DeleteDraftTaskService>();
+builder.Services.AddScoped<IDraftTaskRepository, DraftTaskRepository>();
 
 //AutoMapper
 builder.Services.AddAutoMapper(typeof(DraftTaskMappingProfile).Assembly);
+
+//Database
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
---
+++ projects/backend/src/Infrastructure/Data/Migrations/20250225193545_InitialDraftTaskTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDraftTaskTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DraftTasks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Duration = table.Column<TimeSpan>(type: "interval", nullable: true),
+                    Priority = table.Column<int>(type: "integer", nullable: false),
+                    Deadline = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DraftTasks", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DraftTasks");
+        }
+    }
+}
